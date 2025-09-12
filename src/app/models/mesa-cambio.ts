1deb3b851883c3f5769d12ec24b1eb691140a072
Maria <mariaaguedez.08@gmail.com>
2025-09-12 15:23:01 -0400

    Refactor: ConsultabcvMesaCambioComponent: Enhance form handling, improve data management, and update styles for better UI/UX. Implement localStorage for persistent filters and results, add selection functionality for table rows, and integrate export feature for Excel files.

--- src/app/models/mesa-cambio.ts
+++ src/app/models/mesa-cambio.ts
@@ -100,3 +100,28 @@ export interface Canje {
   ID_JORNADA: string;
   ID_BCV: string;
 }
+export interface ConsultaBcv {
+  ID: number;
+  TIPO_OPER: string;
+  TIPO_PER: string;
+  RIF_CLIENTE: number;
+  NOMBRE_CLIENTE: string;
+  CODIGO_MONEDA_ENT: string;
+  MONTO_ENT: number;
+  TIPO_INSTRU_ENT: string;
+  FECHA: string; // Puede ser Date si se requiere
+  CUENTA_ME_ENT: string;
+  CUENTA_MN_ENT: string;
+  CODIGO_MONEDA_REC: string;
+  MONTO_REC: number;
+  TIPO_INSTRU_REC: string;
+  CUENTA_ME_REC: string;
+  CUENTA_MN_REC: string;
+  MONTO_REC_BCV: number;
+  MONTO_CANJE_BASE: number;
+  TIPO_PACTO: string;
+  OBSERVACION: string;
+  ESTATUS: string;
+  ID_JORNADA: string;
+  ID_BCV: string;
+}
