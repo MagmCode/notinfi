8681ddf8e66df6babd9847b7d19995d733ad0820
Maria <mariaaguedez.08@gmail.com>
2025-10-17 15:03:50 -0400

    fix: Update environment URLs for desarrollo and local environments in workflow

--- src/environments/environment.desarrollo.ts
+++ src/environments/environment.desarrollo.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    urlEndPoint: 'http://180.183.67.228:8084/',
-    urlEndPoint2: 'http://180.183.67.228:8082/',
+    urlEndPoint: 'http://180.183.172.74:8084/',
+    urlEndPoint2: 'http://180.183.172.74:8082/',
     baseUrl: '/asi/',
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,8 +11,8 @@
 // BACKEND URL
 export const environment = {
     production: false,
-    urlEndPoint: 'http://180.183.67.228:8084/',
-    urlEndPoint2: 'http://180.183.67.228:8082/',
+    urlEndPoint: 'http://host.docker.internal:8084/',
+    urlEndPoint2: 'http://host.docker.internal:8082/',
     baseUrl: '/',
 };
 /*
